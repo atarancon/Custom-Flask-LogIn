cce50ad00b053e4bf774d9b9a848497c44024b7e
Alexander Tarancon <ataranco@umail.iu.edu>
2019-12-07 19:21:47 -0500

    fixed username label

--- myproject/forms.py
+++ myproject/forms.py
@@ -12,7 +12,7 @@ class LoginForm(FlaskForm):
    
 class RegistrationForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
-    username = StringField('Usernme',validators=[DataRequired()])
+    username = StringField('Username',validators=[DataRequired()])
     password = PasswordField('Password',validators=[DataRequired(),EqualTo('pass_confirm',message='Passwords must match!')])
     pass_confirm = PasswordField('Confirm Password',validators=[DataRequired()])
     submit = SubmitField('Register!')
